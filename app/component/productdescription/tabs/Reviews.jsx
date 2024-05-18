2b7981180135dbb9a66b69c56acb670f9c83ee34
Hussain101 <hussainsidd88@gmail.com>
2024-05-18 22:49:21 +0500

    Review tab section done

--- app/component/productdescription/tabs/Reviews.jsx
+++ app/component/productdescription/tabs/Reviews.jsx
@@ -1,6 +1,24 @@
-import { Box, Flex, Image, Progress, Text } from '@mantine/core'
+import { Box, Button, Card, Flex, Group, Image, Progress, Rating, Text } from '@mantine/core'
 import React from 'react'
-
+const reviews = [
+    {
+      rating: 4,
+      title: 'A Solid Choice for the Budget-Conscious',
+      reviewer: '{name}',
+      date: '{date}',
+      verified: true,
+      content: `If you're in the market for an affordable TV that delivers decent performance, the Insignia 55" Class F30 Series LED 4K UHD Smart Fire TV might just be the one for you. While it's not the most premium option available, it has some standout features that make it a viable choice for those looking to save a few bucks without sacrificing too much on quality.`,
+    },
+    {
+      rating: 4,
+      title: 'A Solid Choice for the Budget-Conscious',
+      reviewer: '{name}',
+      date: '{date}',
+      verified: true,
+      content: `If you're in the market for an affordable TV that delivers decent performance, the Insignia 55" Class F30 Series LED 4K UHD Smart Fire TV might just be the one for you. While it's not the most premium option available, it has some standout features that make it a viable choice for those looking to save a few bucks without sacrificing too much on quality.`,
+    },
+  ];
+  
 const Reviews = () => {
   return (
     <Box mt={44}>
@@ -73,6 +91,21 @@ const Reviews = () => {
               </Flex>
             </Flex>
         </Flex>
+        <Box>
+        {reviews.map((review, index) => (
+        <Card key={index} shadow="sm" padding="lg" mb="lg" withBorder>
+          <Group align="center">
+            <Rating value={review.rating} readOnly />
+          </Group>
+          <Text weight={700} size="lg" mt="sm">{review.title}</Text>
+          <Text size="sm" color="dimmed" mt="xs">
+            Reviewed by {review.reviewer} on {review.date} {review.verified && '- Verified Purchase'}
+          </Text>
+          <Text mt="md">{review.content}</Text>
+          <Text fz={14} mt={12} style={{textDecoration:"underline"}}>Read More</Text>
+        </Card>
+      ))}
+        </Box>
     </Box>
   )
 }
